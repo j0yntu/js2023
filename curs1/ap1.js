c337bd2d1c42d1c583e4e4a00da7963b55da35aa
Marian <117385905+j0yntu@users.noreply.github.com>
2022-12-08 18:54:13 +0000

    js

---
+++ curs1/ap1.js
@@ -0,0 +1,7 @@
+
+alert('Salut Javascript');
+window.alert('Salut!');
+confirm('Vreau sa invat JavaScript');
+console.log('Mesaj in Consola');
+console.info('Info Status');
+console.error('Mesaj de eroare');
